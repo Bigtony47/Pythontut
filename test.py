fccb36ebd44a71744367ab3d56e783379fe6e478
tony_f22 <tonydiazrivera123@gmail.com>
2026-04-20 17:27:10 -0400

    lasagna project working version

--- test.py
+++ test.py
@@ -1,32 +1,20 @@
 EXPECTED_BAKE_TIME = 40
-print(EXPECTED_BAKE_TIME)
+
 def bake_time_remaining(elapsed_bake_time):
     return EXPECTED_BAKE_TIME - elapsed_bake_time
+
 def preparation_time_in_minutes(number_of_layers):
     return number_of_layers * 2
+
 def elapsed_time_in_minutes(number_of_layers, elapsed_bake_time):
     return preparation_time_in_minutes(number_of_layers) + elapsed_bake_time
-print(elapsed_time_in_minutes(3, 20))
-"""Calculate the elapsed cooking time.              
-        :param number_of_layers: int - the number of layers in the lasagna.
-        :param elapsed_bake_time: int - elapsed cooking time.
-        :return: int - total time elapsed (in minutes) preparing and cooking.
-        this Function takes two integers representing the number of lasagna
-layers and the
-````````time already spent baking and calculates the total elapsed minutes
-spent cooking the
-        lasagna.
-       """ 
-print("Bake time remainig dor 30 mins:")
-print(bake_time_remaining(30))
 
-print("Prep time for 3 layers:")
-print(preparation_time_in_minutes(3))
+elapsed = int(input("How many minutes has it been baking? "))
+layers = int(input("How many layers? "))
+remaining = bake_time_remaining(elapsed)
+prep = preparation_time_in_minutes(layers)
+total = elapsed_time_in_minutes(layers, elapsed)
 
-print("Total time for 3 layers:")
-print(preparation_time_in_minutes(3))
-
-print("Total elapsed time (3 layers, 20 mins in oven):")
-print(elapsed_time_in_minutes(3, 20))
-
-# I really hope i could make you proud, big bro.
+print("Time left:", remaining)
+print("Prep time:", prep)
+print("Total time:", total)
